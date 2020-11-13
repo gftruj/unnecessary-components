d84578dab7b256e190666f2a369bc7d1acb2f94c
gftruj <gftruj@gmail.com>
2020-11-13 19:34:17 +0100

    asourcemap fixes

--- src/asourcemap/polygonutils.js
+++ src/asourcemap/polygonutils.js
@@ -1,23 +1,21 @@
-module.exports = PolygonUtils = (function() {
-    function isPointInPolygon(point, vs) {
-        // ray-casting algorithm based on
-        // https://github.com/substack/point-in-polygon
-        // https://wrf.ecse.rpi.edu/Research/Short_Notes/pnpoly.html/pnpoly.html
+module.exports = PolygonUtils = function isPointInPolygon(point, vs) {
+  // ray-casting algorithm based on
+  // https://github.com/substack/point-in-polygon
+  // https://wrf.ecse.rpi.edu/Research/Short_Notes/pnpoly.html/pnpoly.html
 
-        var x = point[0],
-          y = point[1];
+  var x = point[0],
+    y = point[1];
 
-        var inside = false;
-        for (var i = 0, j = vs.length - 1; i < vs.length; j = i++) {
-          var xi = vs[i][0],
-            yi = vs[i][1];
-          var xj = vs[j][0],
-            yj = vs[j][1];
+  var inside = false;
+  for (var i = 0, j = vs.length - 1; i < vs.length; j = i++) {
+    var xi = vs[i][0],
+      yi = vs[i][1];
+    var xj = vs[j][0],
+      yj = vs[j][1];
 
-          var intersect =
-            yi > y != yj > y && x < ((xj - xi) * (y - yi)) / (yj - yi) + xi;
-          if (intersect) inside = !inside;
-        }
-        return inside;
-      }
-})
+    var intersect =
+      yi > y != yj > y && x < ((xj - xi) * (y - yi)) / (yj - yi) + xi;
+    if (intersect) inside = !inside;
+  }
+  return inside;
+}
